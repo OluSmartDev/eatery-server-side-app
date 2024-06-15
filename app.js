c99c8782d81f550b41dae327ab4d10e509483c07
OluSmartDev <oolalere2015@gmail.com>
2024-06-15 22:18:12 +0100

    Implemented handlers middlewares in app

--- app.js
+++ app.js
@@ -4,9 +4,13 @@ const app = express();
 
 // const PORT = process.env.PORT || 3000;
 const {PORT} = require("./config/envConfig");
+const { notFound, errorHandler } = require("./middlewares/handlers");
 
 require("./operations/routes")(app);
 require("./operations/db")(app);
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(PORT, () => {
     console.log(`Eatery App listening on port: ${PORT} `);
