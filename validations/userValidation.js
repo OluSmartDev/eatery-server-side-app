c7077ff7baddd98504f280b727f3ff232e97ba4d
OluSmartDev <oolalere2015@gmail.com>
2024-06-18 11:54:58 +0100

    Implemented signUpValidator in userValidation.js

---
+++ validations/userValidation.js
@@ -0,0 +1,33 @@
+const Joi = require("joi");
+const StatusCodes = require("../utils/statusCodes");
+
+const validateOptions = {
+    abortEarly: false,
+    allowUnknown: true,
+    stripUnknown: true
+};
+const signUpValidator = (req, res, next) => {
+
+    const schema = Joi.object({
+        first_name: Joi.string().min(3).max(20).required(),
+        last_name: Joi.string().min(3).max(20).required(),
+        username: Joi.string().required(),
+        email: Joi.string().email().required(),
+        password: Joi.string().min(8).required()
+    });
+
+    const result = schema.validate(req.body, validateOptions);
+
+    if (result.error) {
+        console.log(result.error);
+
+        // return res.send(error.details);
+        return res.status(StatusCodes.BAD_REQUEST).json({
+            status: false,
+            msg: "Invalid Request"
+        });
+    }
+    next();
+} 
+
+module.exports = {signUpValidator}
