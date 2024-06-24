364b4698e48c1add7ef01418545f6ff820e83eb3
OluSmartDev <oolalere2015@gmail.com>
2024-06-24 13:01:14 +0100

    Orders Processing: implemented processOrder and other CRUD endpoints

--- controllers/OrdersController.js
+++ controllers/OrdersController.js
@@ -56,4 +56,121 @@ exports.createOrder = async (req, res, next) => {
             msg: "Invalid Request"
         });
     }
+    next();
+}
+
+exports.processOrder = async (req, res, next) => {
+    try {
+
+        const { _id } = req.params; 
+
+        const orderExist = await orderModel.findOne({_id: _id, order_completed: false}); 
+
+        // TO DO: Add logic to calculate total amount of order
+
+        // const orderedItem = await MenuModel.findOne({dish_name: orderExist.dish_name});
+
+        // const { dish_name, description, price } = orderedItem;  
+ 
+        if (!orderExist) {
+            return res.status(StatusCodes.NOT_FOUND).json({
+            status: false,
+            msg: "Order ID does not exist"
+            });
+        }
+  
+      // Update order status
+      orderExist.order_completed = true;
+  
+      const processedOrder = await orderExist.save();
+      
+      console.log("Ordered item processed successfully");
+      return res.status(StatusCodes.OK).json({
+        status: true,
+        msg: "Ordered item processed successfully",
+        data: processedOrder
+      });
+    } catch (error) {
+      console.log("ProceeOrder Error: ", error);
+      return res.status(StatusCodes.BAD_REQUEST).json({
+        status: false,
+        msg: "Invalid Request"
+      });
+    }
+}
+
+exports.getAllOrders = async (req, res, next) => {
+    try {
+      // Find all order items
+        const orders = await orderModel.find();
+        
+        console.log("All Orders retrieved successfully");
+        return res.status(StatusCodes.OK).json({
+            status: true,
+            msg: "All Orders retrieved successfully",
+            data: orders
+        });
+    } catch (error) {
+      console.log("GetAllOrders Error: ", error);
+      return res.status(StatusCodes.BAD_REQUEST).json({
+        status: false,
+        msg: "Invalid Request"
+    });
+    }
+}
+
+exports.getOrderItem = async (req, res, next) => {
+    try {
+    const { _id } = req.params; 
+
+        const orderExist = await orderModel.findById(_id);
+ 
+      if (!orderExist) {
+        return res.status(StatusCodes.NOT_FOUND).json({
+          status: false,
+          msg: "Order item not found"
+        });
+      };
+      
+      console.log("Order item retrieved successfully");
+      return res.status(StatusCodes.OK).json({
+        status: true,
+        msg: "Order item retrieved successfully",
+        data: orderExist
+      });
+
+    } catch (error) {
+      console.log("GetOrderItem Error: ", error);
+      return res.status(StatusCodes.BAD_REQUEST).json({
+        status: false,
+        msg: "Invalid Request"
+      });
+    }
+}
+
+exports.deleteOrderItem = async (req, res, next) => {
+    try {
+        const { _id } = req.params; 
+
+        const orderExist = await orderModel.findByIdAndDelete(_id);
+   
+      if (!orderExist) {
+        return res.status(StatusCodes.NOT_FOUND).json({
+          status: false,
+          msg: "Order item not found"
+        });
+      }
+  
+      console.log("Order item deleted successfully");
+      return res.status(StatusCodes.OK).json({
+        status: true,
+        msg: "Order item deleted successfully"
+      });
+    } catch (error) {
+      console.log("DeleteOrderItem Error: ", error);
+      return res.status(StatusCodes.BAD_REQUEST).json({
+        status: false,
+        msg: "Invalid Request"
+      });
+    }
 }
--- routes/ordersRouter.js
+++ routes/ordersRouter.js
@@ -1,9 +1,13 @@
 const express = require("express");
-const { createOrder } = require("../controllers/OrdersController");
+const { createOrder, processOrder, getAllOrders, getOrderItem, deleteOrderItem } = require("../controllers/OrdersController");
 const { createOrderValidator } = require("../validations/userValidation");
 
 const router = express.Router();
 
 router.post("/createorder", createOrderValidator, createOrder);
+router.put("/:_id", processOrder);
+router.get("/allorders", getAllOrders);
+router.get("/:_id", getOrderItem);
+router.delete("/:_id", deleteOrderItem);
 
 module.exports = router;
